c4aea939ed382c4cd72b0fd1a5db179bed04284d
ryfazrin <riansera515@gmail.com>
2025-06-15 21:45:08 +0700

    fix(config): update entry point and output names in package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter',
+      entry: './src/main.ts',
+      name: 'Main',
+      fileName: 'main',
     },
   }
 })
